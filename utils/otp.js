d6386d6765a8463652cc929551c969ff1fb4ad5b
Prabhudda-Goudar <prabhudda@slikk.club>
2025-02-10 22:51:21 +0530

    worked on api

--- utils/otp.js
+++ utils/otp.js
@@ -3,7 +3,6 @@ import twilioClient from "../config/twilio.js";
 export const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();
 
 export const sendOTPWhatsApp = async (phoneNumber, otp) => {
-  console.log(phoneNumber)
   await twilioClient.messages.create({
     from: `whatsapp:${process.env.TWILIO_WHATSAPP_NUMBER}`,
     to: `whatsapp:${phoneNumber}`,
